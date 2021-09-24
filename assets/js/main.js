030796ce7f5ea65588146097e30d179d920a8d49
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-09-24 00:54:51 -0700

    Broke animation into discrete steps, changed animation handling in player.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 import * as THREE from './ThreeJS/three/build/three.module.js';
-import { Player } from './player.js';
-// console.log("main.js loaded");
+import { Player } from './Player.js';
 
 
 // Get the bounds of the game from the window
@@ -10,14 +9,15 @@ var height = gameSpace.offsetHeight;
 
 console.log("Game Space size: " + width + " x " + height);
 
-// Create camera, scene, and renderer for three.js
+// Create three.js scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8f7ff);
 
-// // DEBUG: Add an axes helper
-// var axesHelper = new THREE.AxesHelper(50);
-// scene.add(axesHelper);
+// DEBUG: Add an axes helper to the scene
+var axesHelper = new THREE.AxesHelper(50);
+scene.add(axesHelper);
 
-// Set up scene lighting
+// set up lighting for the scene
 var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 scene.add(ambientLight);
 
@@ -25,62 +25,35 @@ var directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
 directionalLight.position.set(-250, 300, -400);
 scene.add( directionalLight );
 
+// Set up three.js camera
 const camera = new THREE.OrthographicCamera(-width/2, width/2, -height/2, height/2, 0, 1000);
 camera.position.set(200, -200, 300);
 camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
-
 camera.updateMatrix();
 
-//scene.add(camera);
-
+// Set up three.js renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 gameSpace.appendChild(renderer.domElement);
 
-// Set the background of the scene to be our blue color);
-scene.background = new THREE.Color(0xa8f7ff);
-
-// TODO: Display a loading screen while loading assets for the game with loading icon
-
-// TODO: Start page with options, credits, and play buttons, and a splash image
-
-// TODO: Display options page
-// TODO: Audio slider
-// TODO: Color Blind mode toggle
-
-// TODO: Display credits
-
-// TODO: Start game
-
-// Create player model
+// Create and add player to the scene
 var player = new Player();
-
 scene.add(player);
 
+// DEBUG: Make the player move every 3 seconds. 
+setInterval( function() {
+    player.beginStep(new THREE.Vector3(0, 1, 0));
+}, 3000);
 
-
-
+// Animation loop
 const animate = function (curTimestamp) {
     requestAnimationFrame(animate);
-    player.animateStep(curTimestamp);
+    player.update();
     renderer.render(scene, camera);
 }
 
+
+
+// Start animation
 animate();
-
-// Add grid to scene (TODO: Built from json level file)
-// Add paint buckets and splashes to scene
-
-// TODO: Main game loop
-// var update = function() {
-//     // TODO: Block controls
-//     // TODO: Block logic
-//     // TODO: Paint bucket logic
-//     // TODO: Grid tile logic
-
-//     // TODO: Check level criteria
-//     // TODO: Record stats 
-//     // TODO: End level screen if not in a tutorial level. otherwise, move to the next level.
-// }
-
--- assets/js/player.js
+++ assets/js/player.js
@@ -1,23 +1,92 @@
 import * as THREE from './ThreeJS/three/build/three.module.js';
+const states = {
+    ONCREATE: "onCreate",
+    STEPPING: "stepping",
+    ONSTEPEND: "onStepEnd",
+    IDLE: "idle"
+}
 
 class Player extends THREE.Mesh {
     constructor() {
         super(new THREE.BoxGeometry(100, 100, 100), new THREE.MeshLambertMaterial({color:0xFFFFFF}));
         this.sideColors = [null, null, null, null, null, null];
         //this.inAnimation = false;
-        this.animationStart;
+        this.animationStart=0;
+        this.animationAxis = new THREE.Vector3(1, 0, 0);
+        this.state=states.ONCREATE;
+        this.clock = new THREE.Clock(false); // Set to false so the clock doesn't run automatically.
+        this.stepTime = 1;
+        this.stepAxis = null;
     }
 
-    animateStep(curTimestamp) {
-        //if(!this.inAnimation) inAnimation = true;
-        // A one second animation, rotating by the x axis by 90 deg (half pi radians)
-        let angle = (Math.PI/2) * (curTimestamp/750);
-        let axis = new THREE.Vector3(1, 0, 0);
-        let radius = Math.sqrt((50) * (50)); //Center of square to corner
-        //console.log(this.width);
-        //let newPos = new THREE.Vector3();
-        this.setRotationFromAxisAngle(axis, angle);
-        this.position.set(0, 0, -Math.abs(radius * Math.sin(angle*2))) 
+    update() {
+        switch(this.state) {
+            case states.ONCREATE:
+                this.#onCreate();
+                break;
+
+            case states.STEPPING:
+                this.#stepping();
+                break;
+
+            default:
+                /// idle
+                break;
+        }
+    }
+
+    // Handles changing rotation and position during animation
+    #animateStep() {
+        let timestamp;
+        if(this.clock.elapsedTime > this.stepTime) timestamp = this.stepTime;
+        else timestamp = this.clock.getElapsedTime();
+
+        if(timestamp < this.stepTime) {
+            // Calculate where the block should be at this part of the animation and move it there
+            let angle = (Math.PI/2) * (timestamp/this.stepTime); // the angle, 90 degrees at maximum
+            let radius = Math.sqrt((50) * (50)); //Center of side to corner
+            
+            this.setRotationFromAxisAngle(this.stepAxis, angle);
+            // let horizontalPos = radius * Math.cos(angle*2);
+            // let horizontalMovementInBothDirections = new THREE.Vector3(horizontalPos, horizontalPos, 0);
+            // let horizontalMovement = horizontalMovementInBothDirections.multiply(this.stepAxis);
+            let verticalMovement = new THREE.Vector3(0, 0, Math.abs(radius * Math.sin(angle*2)));
+            // let newPosition = horizontalMovement.add(verticalMovement);
+            // console.log(newPosition);
+            this.position.set(0, 0, -verticalMovement.z); 
+        } else {
+            // Move the block to it's final position
+            let angle = Math.PI/2;
+            // let horizontalPos = radius * Math.cos(angle*2);
+            // let horizontalMovementInBothDirections = new THREE.Vector3(horizontalPos, horizontalPos, 0);
+            // let horizontalMovement = horizontalMovementInBothDirections.multiply(this.stepAxis);
+            this.position.set(0, 0, 0);
+
+            this.state = states.IDLE;
+            this.clock.stop();
+        }
+    }
+
+    #onCreate() {
+        //Code for when the object is created
+    }
+
+    #stepping() {
+        // Code for while the player is stepping
+        this.#animateStep();
+    }
+
+    #onStepEnd() {
+        // Code for directly after the player finishes stepping
+        // Will be used later to calculate whether this step paints a square
+    }
+
+    // A method called to make the player step in a cardinal direction
+    // axis is a Vector3 where 1 is the value of the axis to use and the others are 0
+    beginStep(axis) {
+        this.state=states.STEPPING;
+        this.clock.start();
+        this.stepAxis = axis;
     }
 }
 
